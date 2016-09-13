09bb1dca1fec0516df3b28fb123cac5613521dbc
Ranjani.R <ranjinirajendran0250@gmail.com>
2016-09-13 14:28:02 +0530

    num.java
    changed file

--- num.java
+++ num.java
@@ -5,15 +5,15 @@ public Static void main(String args[])
 {
 Scanner input=new Scanner(System.in);
 int num=input.nextInt();
-if(num>0)
+if(num > 0)
 {
 System.out.println("Positive number");
 }
-else if(num<0)
+else if(num < 0)
 {
 System.out.println("Negative number");
 }
-else if(num==0)
+else if(num == 0)
 {
 System.out.println("Zero");
 }
